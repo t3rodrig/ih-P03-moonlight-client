4de79b7fdb5a65ae9bbd9fb6117b5ed04f03ebc6
Tonalli <tonalli@xanum.uam.mx>
2020-04-04 12:25:41 -0600

    Add react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App">
       <Header />
       <Navbar />
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
     </div>
   );
 }
