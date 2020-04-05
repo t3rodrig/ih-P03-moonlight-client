5fcc5ae54594e8b59066905880d39796ec1a0f66
Tonalli <tonalli@xanum.uam.mx>
2020-04-05 13:48:19 -0500

    Add Product components

---
+++ src/components/products/ProductGrid.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+import ProductThumbnail from './ProductThumbnail';
+import products from './products.json';
+
+class ProductGrid extends Component {
+  constructor() {
+    super();
+    this.state = { listOfProducts: products};
+  }
+
+  render() {
+    const listOfProducts = this.state.listOfProducts;
+    console.log(listOfProducts);
+    return (
+      <section id="products">
+        <div className="row">
+          { listOfProducts.map((eachProduct) => {
+            return (
+              <div className="col-md-4" key={eachProduct._id}>
+                <ProductThumbnail product={eachProduct}/>
+              </div>
+            )
+          }) }
+        </div>
+      </section>
+    );
+  }
+}
+
+export default ProductGrid;
---
+++ src/components/products/ProductThumbnail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProductThumbnail = (props) => {
+  const { product } = props;
+  return (
+    <div className="thumbnail">
+      <img src={product.imageURL} alt="Product"/>
+      <div className="caption">
+        <h4 className="pull-right">
+          {`$${product.price}`}
+        </h4>
+        <h4>{product.name}</h4>
+      </div>
+      <div className="ratings">
+        <p className="pull-right">4 Reviews</p>
+        <p>
+        <img src="/images/star-fill.svg" alt="" width="14"/>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductThumbnail;
