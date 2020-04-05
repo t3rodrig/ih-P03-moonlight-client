03780891f3e6fc231c7bf66f3976a61a0a3f901c
Tonalli <tonalli@xanum.uam.mx>
2020-04-05 13:59:49 -0500

    Fix ProductThumbnail and update App.css

--- src/components/products/ProductThumbnail.js
+++ src/components/products/ProductThumbnail.js
@@ -4,7 +4,7 @@ const ProductThumbnail = (props) => {
   const { product } = props;
   return (
     <div className="thumbnail">
-      <img src={product.imageURL} alt="Product"/>
+      <img src={product.imageURL} alt="Product" className="thumbnail"/>
       <div className="caption">
         <h4 className="pull-right">
           {`$${product.price}`}
@@ -14,7 +14,7 @@ const ProductThumbnail = (props) => {
       <div className="ratings">
         <p className="pull-right">4 Reviews</p>
         <p>
-        <img src="/images/star-fill.svg" alt="" width="14"/>
+        <img src="/images/star-fill.svg" alt="" className="star"/>
         </p>
       </div>
     </div>
