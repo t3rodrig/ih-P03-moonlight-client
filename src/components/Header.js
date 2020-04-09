4182370139718ad2a12f8e0e52b7222d7824d50a
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-09 18:18:55 -0500

    Update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,17 @@ const Header = () => {
   return (
     <header>
       <div className="row">
-        <div className="col-md-4 col-sm-12">
+        <div className="col-sm-6 col-md-4">
           <h1 className="logo"><img src="/images/tag.svg" alt="" width="36" height="36" title="tag"/><span>Moonlight</span></h1>
           <p className="subtext">The moon is a loyal companion</p>
         </div>
-        <div className="col-md-8 col-sm-12">
+        <div className="col-sm-6 col-md-8">
           <ul className="nav justify-content-end">
             <li className="nav-item">
-              <a className="nav-link" href="#"> <IoIosLogIn size="3em" color="#C83C27"/> </a>
+              <a className="nav-link" href="#"> <IoIosLogIn title="Login" size="3em" color="#000"/> </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#"> <IoMdCart size="3em" color="#C83C27"/> </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#"> <IoMdCart title="Cart" size="3em" color="#000"/> </a>
             </li>
           </ul>
         </div>
