b2bd4ed9c925f09e1e60afdcc3d6a54116d51c80
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-22 15:20:43 -0500

    Roll back HomPage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,12 @@
 import React, {Fragment} from 'react';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
 import Jumbotron from '../components/Jumbotron';
-import Footer from '../components/Footer';
 import ProductGrid from "../components/products/ProductGrid";
 
 const Home = () => {
   return (
     <Fragment>
-      <Header />
-      <Navbar />
       <Jumbotron />
       <ProductGrid />
-      <Footer />
     </Fragment>
   );
 };
