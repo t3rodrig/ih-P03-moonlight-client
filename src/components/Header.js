6e9b5fb213717ff780b0edd9db4e6063c32c022c
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-23 16:02:33 -0500

    Update header
    in order to handle loggedInUser

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosLogIn, IoMdCart, IoIosLogOut } from "react-icons/io";
+import AuthService from '../services/auth-service';
 
 class Header extends Component {
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({...this.state, loggedInUser: nextProps['userInSession']});
+  }
+
+  logoutUser = () => {
+    this.service.logout()
+    .then(() => {
+      this.setState({ loggedInUser: null });
+      this.props.getUser(null);
+    });
+  }
 
   render() {
     return (
@@ -15,10 +33,12 @@ class Header extends Component {
           <div className="col-sm-6 col-md-8">
             <ul className="nav justify-content-end">
               <li className="nav-item">
+              {
+                this.state.loggedInUser ? 
+                <Link to="/logout" className="nav-link"> <IoIosLogOut onClick={() => this.logoutUser()} title="Logout" size="3em" color="#000"/> </Link>
+                :
                 <Link to="/login" className="nav-link"> <IoIosLogIn title="Login" size="3em" color="#000"/> </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/logout" className="nav-link"> <IoIosLogOut title="Logout" size="3em" color="#000"/> </Link>
+              }
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#"> <IoMdCart title="Cart" size="3em" color="#000"/> </a>
